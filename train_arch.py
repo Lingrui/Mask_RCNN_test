c1eec60aaac119a47dc744b98ab60974da15239f
Lingrui <lingruicai@gmail.com>
2018-02-11 21:49:14 -0500

    add test scripts

---
+++ train_arch.py
@@ -0,0 +1,358 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+from config import Config
+import utils
+import model as modellib
+import visualize
+from model import log
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# ## Configurations
+# In[2]:
+class ArchConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "arch"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 3  # background + 3 shapes
+    IMAGE_MIN_DIM = 1920
+    IMAGE_MAX_DIM = 2560
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 32
+
+    # Use a small epoch since the data is simple
+
+    # use small validation steps since the epoch is small
+    
+config = ArchConfig()
+config.display()
+
+iter_num=0 
+# ## Notebook Preferences
+
+# In[3]:
+
+def get_ax(rows=1, cols=1, size=8):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
+class ArchDataset(utils.Dataset):
+    """Generates the shapes synthetic dataset. The dataset consists of simple
+    shapes (triangles, squares, circles) placed randomly on a blank surface.
+    The images are generated on the fly. No file access required.
+    """
+    #how many instance in the image=
+    def get_obj_index(self,image):
+        n = np.max(image)
+        return n
+
+    # yaml from labelme    
+    def from_yaml_get_class(self,image_id):
+        info = self.image_info[image_id]
+        with open(info['yaml_path']) as f:
+            temp = yaml.load(f.read())
+            labels = temp['label_names']
+            del label[0]
+        return labels 
+
+    def draw_mask(self, num_obj, mask, image_id):
+        info = self.image_info[image_id]
+        for index in range(num_obj):
+            for i in range(info['width']):
+                for j in range(info['height']):
+                    at_pixel = image.getpixel((i, j))
+                    if at_pixel == index + 1:
+                        mask[j, i, index] =1
+        return mask
+
+    def load_shapes(self, count, height, width, img_floder, mask_floder, imglist,dataset_root_path):
+        """Generate the requested number of synthetic images.
+        count: number of images to generate.
+        height, width: the size of the generated images.
+        """
+        # Add classes
+        self.add_class("shapes", 1, "yam")
+        self.add_class("shapes", 2, "milet")
+        self.add_class("shapes", 3, "redbean")
+
+        # Add images
+        # Generate random specifications of images (i.e. color and
+        # list of shapes sizes and locations). This is more compact than
+        # actual images. Images are generated on the fly in load_image().
+        for i in range(count):
+            #print("i= ", i)
+            filestr = imglist[i].split(".")[0]
+            #print("filestr: ",filestr)
+            maskstr = filestr.split("_")[0]
+            #print("filestr: ",filestr)
+            mask_path = mask_folder + "/" + maskstr + ".png"
+            yaml_path=dataset_root_path+"arch_mask/"+maskstr+"_json/info.yaml"
+            self.add_image("shapes", image_id=i, path=img_folder + "/" + imglist[i],
+                           width=width, height=height, mask_path=mask_path,yaml_path=yaml_path)
+    '''
+    def load_image(self, image_id):
+        """Generate an image from the specs of the given image ID.
+        Typically this function loads the image from a file, but
+        in this case it generates the image on the fly from the
+        specs in image_info.
+        """
+        info = self.image_info[image_id]
+        bg_color = np.array(info['bg_color']).reshape([1, 1, 3])
+        image = np.ones([info['height'], info['width'], 3], dtype=np.uint8)
+        image = image * bg_color.astype(np.uint8)
+        for shape, color, dims in info['shapes']:
+            image = self.draw_shape(image, shape, dims, color)
+        return image
+    '''
+    def image_reference(self, image_id):
+        """Return the shapes data of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "shapes":
+            return info["shapes"]
+        else:
+            super(self.__class__).image_reference(self, image_id)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for shapes of the given image ID.
+        """
+        global iter_num
+        info = self.image_info[image_id]
+        count = 1  # number of object
+        #print(info['mask_path'])
+        img = cv2.imread(info['mask_path'])
+        num_obj = self.get_obj_index(img)
+        #print ("+"*50,"\n",info['height'],"\n",info['width'],"\n",img)
+        mask = np.zeros([info['height'], info['width'], num_obj], dtype=np.uint8)
+        mask = self.draw_mask(num_obj, mask, image_id)
+        occlusion = np.logical_not(mask[:, :, -1]).astype(np.uint8)
+        for i in range(count - 2, -1, -1):
+            mask[:, :, i] = mask[:, :, i] * occlusion
+            occlusion = np.logical_and(occlusion, np.logical_not(mask[:, :, i]))
+        labels=[]
+        labels=self.from_yaml_get_class(image_id)
+        labels_form=[]
+        for i in range(len(labels)):
+            if labels[i].find("yam")!=-1:
+                #print "box"
+                labels_form.append("yam")
+            elif labels[i].find("milet")!=-1:
+                #print "column"
+                labels_form.append("milet")
+            elif labels[i].find("redbean")!=-1:
+                #print "package"
+                labels_form.append("redbean")
+        class_ids = np.array([self.class_names.index(s) for s in labels_form])
+        return mask, class_ids.astype(np.int32)
+# In[5]:
+
+dataset_root_path="/shared/s2/users/lcai/Mask_RCNN/"
+img_folder = dataset_root_path+"arch"
+mask_folder = dataset_root_path+"arch_mask"
+#yaml_folder = dataset_root_path
+imglist = os.listdir(img_folder)
+count = len(imglist)
+width = 2560
+height = 1920
+
+# Training dataset
+dataset_train = ArchDataset()
+dataset_train.load_shapes(count, 1920, 2560,img_folder, mask_folder, imglist,dataset_root_path)
+dataset_train.prepare()
+
+# Validation dataset
+dataset_val = ArchDataset()
+dataset_val.load_shapes(count, 1920, 2560,img_folder, mask_folder, imglist,dataset_root_path)
+dataset_val.prepare()
+
+
+# In[6]:
+# Load and display random samples
+'''
+image_ids = np.random.choice(dataset_train.image_ids, 4)
+for image_id in image_ids:
+    image = dataset_train.load_image(image_id)
+    mask, class_ids = dataset_train.load_mask(image_id)
+    visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
+'''
+# ## Ceate Model
+
+# In[ ]:
+
+# Create model in training mode
+model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir=MODEL_DIR)
+
+# In[7]:
+init_with = "coco"  # imagenet, coco, or last
+
+if init_with == "imagenet":
+    model.load_weights(model.get_imagenet_weights(), by_name=True)
+elif init_with == "coco":
+    # Load weights trained on MS COCO, but skip layers that
+    # are different due to the different number of classes
+    # See README for instructions to download the COCO weights
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])
+elif init_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last()[1], by_name=True)
+
+
+# ## Training
+# 
+# Train in two stages:
+# 1. Only the heads. Here we're freezing all the backbone layers and training only the randomly initialized layers (i.e. the ones that we didn't use pre-trained weights from MS COCO). To train only the head layers, pass `layers='heads'` to the `train()` function.
+# 
+# 2. Fine-tune all layers. For this simple example it's not necessary, but we're including it to show the process. Simply pass `layers="all` to train all layers.
+
+# In[8]:
+
+
+# Train the head branches
+# Passing layers="heads" freezes all layers except the head
+# layers. You can also pass a regular expression to select
+# which layers to train by name pattern.
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE, 
+            epochs=1, 
+            layers='heads')
+# In[9]:
+
+# Fine tune all layers
+# Passing layers="all" trains all layers. You can also 
+# pass a regular expression to select which layers to
+# train by name pattern.
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE / 10,
+            epochs=2, 
+            layers="all")
+
+# In[10]:
+
+# Save weights
+# Typically not needed because callbacks save after every epoch
+# Uncomment to save manually
+# model_path = os.path.join(MODEL_DIR, "mask_rcnn_shapes.h5")
+# model.keras_model.save_weights(model_path)
+
+
+# ## Detection
+
+# In[11]:
+
+
+class InferenceConfig(ArchConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+inference_config = InferenceConfig()
+
+# Recreate the model in inference mode
+model = modellib.MaskRCNN(mode="inference", 
+                          config=inference_config,
+                          model_dir=MODEL_DIR)
+
+# Get path to saved weights
+# Either set a specific path or find last trained weights
+# model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+model_path = model.find_last()[1]
+
+# Load trained weights (fill in path to trained weights here)
+assert model_path != "", "Provide path to trained weights"
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
+
+# In[12]:
+# Test on a random image
+image_id = random.choice(dataset_val.image_ids)
+original_image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset_val, inference_config, 
+                           image_id, use_mini_mask=False)
+
+log("original_image", original_image)
+log("image_meta", image_meta)
+log("gt_class_id", gt_class_id)
+log("gt_bbox", gt_bbox)
+log("gt_mask", gt_mask)
+
+visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id, 
+                            dataset_train.class_names, figsize=(8, 8))
+
+
+# In[13]:
+results = model.detect([original_image], verbose=1)
+r = results[0]
+visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'], 
+                            dataset_val.class_names, r['scores'], ax=get_ax())
+
+
+# ## Evaluation
+
+# In[14]:
+
+
+# Compute VOC-Style mAP @ IoU=0.5
+# Running on 10 images. Increase for better accuracy.
+image_ids = np.random.choice(dataset_val.image_ids, 10)
+APs = []
+for image_id in image_ids:
+    # Load image and ground truth data
+    image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset_val, inference_config,
+                               image_id, use_mini_mask=False)
+    molded_images = np.expand_dims(modellib.mold_image(image, inference_config), 0)
+    # Run object detection
+    results = model.detect([image], verbose=0)
+    r = results[0]
+    # Compute AP
+    AP, precisions, recalls, overlaps =        utils.compute_ap(gt_bbox, gt_class_id,
+                         r["rois"], r["class_ids"], r["scores"])
+    APs.append(AP)
+    
+print("mAP: ", np.mean(APs))
+
